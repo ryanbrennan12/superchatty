59c61432f7334b768b00d9dcfd976ee176fcc83d
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-05-11 22:36:13 -0700

    enpoints

--- clone/index.js
+++ clone/index.js
@@ -6,8 +6,12 @@ import { graphqlExpress } from 'apollo-server-express';
 
 const app = express();
 
+app.use('/graphiql', graphiqlExpress({
+  endPointURL: '/graphql',
+}));
+//
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
 
 
 
-server.listen(8080)
+server.listen(3000);
