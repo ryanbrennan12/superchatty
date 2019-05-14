89bd3a657c2d3815dba1f4e6dc4306fac0120d84
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-05-14 12:14:39 -0700

    working on TeamType to pass

--- clone/schema.js
+++ clone/schema.js
@@ -1,7 +1,5 @@
-
 const graphql = require('graphql');
 const axios = require('axios');
-
 
 const {
   GraphQLObjectType,
@@ -11,32 +9,35 @@ const {
   //GraphQLSchema takes in a root query and returns a GraphQLSchema  instance
 } = graphql;
 
-const CompanyType = new GraphQLObjectType({
-  name: 'Company',
-  fields: {
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-  }
-})
+
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
-    firstName: { type: GraphQLString },
-    age: { type: GraphQLInt },
-    company: {
-      type: CompanyType,
+    username: { type: GraphQLString },
+    password: { type: GraphQLString },
+   }
+ });
+ const TeamType = new GraphQLObjectType({
+  name: 'Team',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    owner: {
+      type: UserType,
       resolve(parentValue, args) {
+        console.log('hett', parentValue, args)
         //parent value is the node on the graph where the query is coming from
 
-       return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+       return axios.get(`http://localhost:3000/users/${parentValue.companyId}`)
        .then(res => res.data)
       }
     }
   }
-});
+})
+
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -50,22 +51,31 @@ const RootQuery = new GraphQLObjectType({
         //resolve(null, { id: 23 })
         return axios.get(`http://localhost:3000/users/${args.id}`)
         .then(resp => resp.data);
-
-
       }
     },
-    company: {
-      type: CompanyType,
+    team: {
+      type: TeamType,
       args: { id: { type: GraphQLString }},
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/companies/${args.id}`)
-        .then(resp => resp.data);
+        return axios.get(`http://localhost:3000/teams/${args.id}`)
+        .then(res => res.data);
       }
     }
   }
 });
 
 
+
+
 module.exports = new GraphQLSchema({
   query: RootQuery
 });
+
+// company: {
+//   type: CompanyType,
+//   args: { id: { type: GraphQLString }},
+//   resolve(parentValue, args) {
+//     return axios.get(`http://localhost:3000/teams/${args.id}`)
+//     .then(resp => resp.data);
+//   }
+// }
